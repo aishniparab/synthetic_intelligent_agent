d64617f6586740dee981bbd9698069926015b0a2
Aishni Parab <aparab@ucsc.edu>
2019-05-30 22:59:25 -0400

    agent takes shortest path to stimuli

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import collections
 
 class Agent:
 	def __init__(self, sensors, connection):
@@ -7,28 +8,47 @@ class Agent:
 		self.speed = 0 #init speed is 0
 		self.pos = (0, 0)
 
-	def test(self):
-		print({"sensors": self.sensors, "connection": self.connection, "speed": self.speed})
+	#def behaviour(self):
+		#if self.connection == "simple":
+
 	
-	def behaviour(self):
-		if self.connection == "simple":
+	def find_path(self, env, start):
+         visited, queue = set([start]), collections.deque([[start]])
+         while queue:
+         	path = queue.popleft()
+         	x, y = path[-1]
+         	if env.grid[y][x] == env.goal:
+         		return path
+         	for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+	            if 0 <= x2 < env.width and 0 <= y2 < env.height and env.grid[y2][x2] != env.wall and (x2, y2) not in visited:
+	                queue.append(path + [(x2, y2)])
+	                visited.add((x2, y2))
 
 class Environment:
-	def __init__(self, size, stimuli, agent):
-		self.grid = [[0 for i in range(size)] for j in range(size)]
-		self.grid[0][0] = stimuli
-		agent.pos = (size/2, size/2)
-		self.grid[size/2][size/2] = "A"
+	def __init__(self, agent):
+		self.grid = [['*', 0, 0, 0, 0], [0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
+		self.wall = "#"
+		self.clear = "."
+		self.goal = "*"
+		self.width = 5
+		self.height = 5
+		self.stimuli_pos = (0,0)
+		agent.pos = (self.width/2, self.height/2)
 	
-	def render(self):
-		pprint(self.grid)
+	def render(self, grid):
+		pprint(grid)
 
 def main():
 	basic = Agent(1, "simple")
-	env = Environment(5, "S", basic)
-	print basic.test()
-	print env.render()
-	print basic.pos
+	env = Environment(basic)
+	
+	env.render(env.grid)
+	path = basic.find_path(env, basic.pos)
+	
+	for pos in path:
+		grid_copy = env.grid[:]
+		grid_copy[pos[0]][pos[1]] = "a"
+		env.render(env.grid) 
 
 if __name__ == "__main__":
     main()
